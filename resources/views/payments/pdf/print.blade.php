c56f9c29da5a04061d123ec46abfff3228cbcc46
Kahina Ouabou <k_ouabou@ITComp.com>
2022-10-17 13:20:36 +0100

    enlver logo dans pdf paiement

--- resources/views/payments/pdf/print.blade.php
+++ resources/views/payments/pdf/print.blade.php
@@ -70,7 +70,7 @@
  <div style='border-bottom: 1px solid;width:100%; height: 170px'>
   <table width="100%">
     <tr>
-       <td valign="top"><img src="{{ public_path('images/logo.png') }}" alt="" width="150"/></td>
+       <td valign="top"></td>
         
         <td align="right">
             <h2>{{$company->name}} </h2>
