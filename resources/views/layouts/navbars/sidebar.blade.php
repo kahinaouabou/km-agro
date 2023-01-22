f485087aa9274a5504555b4912950d5330b7e9ef
kahinaouabou <k_ouabou@esi.dz>
2023-01-22 09:11:34 +0100

    acces program comptable

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -159,6 +159,12 @@
             @endif
             
             @if (auth()->user()->can('payment-list'))
+            <li class="nav-item{{ $activePage == 'program' ? ' active' : '' }}">
+              <a class="nav-link" href="{{ route('programs.index') }}">
+              <i class="material-icons icon-sidebar"> Pt </i>
+                <span class="sidebar-normal">{{ __('Programs') }} </span>
+              </a>
+            </li>
             <li class="nav-item{{ $activePage == 'paymentCategory' ? ' active' : '' }}">
               <a class="nav-link" href="{{ route('paymentCategories.index') }}">
               <i class="material-icons icon-sidebar"> Pc </i>
@@ -179,12 +185,7 @@
                 <span class="sidebar-normal">{{ __('Company') }} </span>
               </a>
             </li>
-            <li class="nav-item{{ $activePage == 'program' ? ' active' : '' }}">
-              <a class="nav-link" href="{{ route('programs.index') }}">
-              <i class="material-icons icon-sidebar"> Pt </i>
-                <span class="sidebar-normal">{{ __('Programs') }} </span>
-              </a>
-            </li>
+            
             <li class="nav-item{{ $activePage == 'discharge' ? ' active' : '' }}">
               <a class="nav-link" href="{{ route('discharges.index') }}">
               <i class="material-icons icon-sidebar"> Ds </i>
